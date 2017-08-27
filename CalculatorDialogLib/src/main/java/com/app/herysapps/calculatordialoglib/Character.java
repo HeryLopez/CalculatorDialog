88b324d8d9bf6c2acd6f3086fda7c9ed708e3788
Hery Lopez <heryjlr@gmail.com>
2017-08-27 18:27:28 +0200

    Add project. First Commit.

--- CalculatorDialogLib/src/main/java/com/app/herysapps/calculatordialoglib/Character.java
+++ CalculatorDialogLib/src/main/java/com/app/herysapps/calculatordialoglib/Character.java
@@ -1,38 +1,33 @@
 package com.app.herysapps.calculatordialoglib;
 
-/**
- * Created by Hery on 10/08/2017.
- */
+class Character {
 
-public class Character {
-
-    public enum Operation {DIVISION, MULTIPLICATION, SUBTRACTION, SUM};
-    public enum Type {NUMBER, OPERATOR };
+    enum Operation {DIVISION, MULTIPLICATION, SUBTRACTION, SUM}
+    enum Type {NUMBER, OPERATOR }
 
     private String mValue;
     private Character.Operation mOperation;
     private Character.Type mType;
 
-    public Character(String value, Character.Operation operation, Character.Type type){
+    Character(String value, Character.Operation operation, Character.Type type){
         mValue = value;
         mOperation = operation;
         mType = type;
     }
 
-    public void setValue(String value){
+    void setValue(String value){
         mValue = value;
     }
 
-    public String getValue() {
+    String getValue() {
         return mValue;
     }
 
-
-    public Character.Type getType() {
+    Character.Type getType() {
         return mType;
     }
 
-    public Character.Operation getOperation(){
+    Character.Operation getOperation(){
         return mOperation;
     }
 }
